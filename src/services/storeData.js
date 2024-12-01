49a4b17f9290dd95ba07f8d83b62bfc8438da1f3
vincensiusadyatma <adyatmav@gmail.com>
2024-12-01 19:30:34 +0700

    fix: fix data store for suggestion data in firestore

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -3,18 +3,21 @@ const { Firestore } = require('@google-cloud/firestore');
 async function storeData(id, result, suggestion) {
     const db = new Firestore();
     
-    // Dapatkan koleksi "predictions" dan dokumen berdasarkan id
+    
     const predictCollection = db.collection('predictions');
     
-    // Struktur data yang akan disimpan di Firestore
+    if (result == 'Non-cancer') {
+        suggestion = "Anda sehat!"
+    }
+  
     const data = {
         id: id,
         result: result,
         suggestion: suggestion,
-        createdAt: new Date().toISOString() // Menambahkan timestamp saat data disimpan
+        createdAt: new Date().toISOString() 
     };
     
-    // Simpan data ke Firestore
+  
     await predictCollection.doc(id).set(data);
     console.log('Data berhasil disimpan di Firestore');
 }
